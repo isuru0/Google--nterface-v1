b9ef4240cf15c45e85c4f76e9c6f1688abc8ea6b
isuru0 <isurushehara@gmail.com>
2023-05-04 20:01:44 +0530

    All files (Already created)

---
+++ action.js
@@ -0,0 +1,21 @@
+// Onload hidden Google Apps
+var appsbox = document.getElementById("googleApps");
+appsbox.style.display = "none";
+
+// Google apps Show and Hidden action
+function apps(){
+    var appsbox = document.getElementById("googleApps");
+    if (appsbox.style.display === "none"){
+        appsbox.style.display = "block";
+    } else if (appsbox.style.display === "block"){
+        appsbox.style.display = "none";
+    }
+}
+
+// Google apps hidden action When click other area
+function hiddenClick(){
+    var appsbox = document.getElementById("googleApps");
+    if (appsbox.style.display = "block"){
+        appsbox.style.display = "none";
+    }
+}
