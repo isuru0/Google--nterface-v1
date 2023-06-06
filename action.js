6d03d0f0e9617b08fcafb37c7c1e7aa187f0b265
isuru0 <isurushehara@gmail.com>
2023-06-06 09:59:07 +0530

    Add buttons links (without Google Apps

--- action.js
+++ action.js
@@ -25,3 +25,36 @@ function searchGoogle(){
     var searchinput = document.getElementById("searchBar").value;
     window.location = "https://www.google.com/search?q=" + (searchinput);
 }
+
+// Link to item links
+function linkabout(){
+    location.href = "https://about.google/?fg=1&utm_source=google-DE&utm_medium=referral&utm_campaign=hp-header";
+}
+function linkstore(){
+    location.href = "https://store.google.com/DE?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=en-DE";
+}
+function linkgmail(){
+    location.href = "https://mail.google.com/mail/&ogbl";
+}
+function linkimages(){
+    location.href = "https://www.google.de/imghp?hl=en&ogbl";
+}
+function linksignin(){
+    location.href = "https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAmgQ";
+}
+
+function linkadvertising(){
+    location.href = "https://www.google.com/intl/en_de/ads/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1";
+}
+function linkbusiness(){
+    location.href = "https://www.google.com/services/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpbfooter&fg=1";
+}
+function linkhsw(){
+    location.href = "https://google.com/search/howsearchworks/?fg=1";
+}
+function linkprivacy(){
+    location.href = "https://policies.google.com/privacy?hl=en-DE&fg=1";
+}
+function linkteams(){
+    location.href = "https://policies.google.com/terms?hl=en-DE&fg=1";
+}
